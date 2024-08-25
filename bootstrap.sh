28e51b7b50101435b557ec3c47b96233b90f9a04
Justin Awrey <awreyjustin@gmail.com>
2024-08-25 13:14:13 -0700

    add boostrap

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# need xcode command line tools and most importantly git before doing anything
+xcode-select --install
+
+# make sure ~/.local/bin is in path before putting chezmoi binary there
+path=("$HOME/.local/bin" $path)
+export PATH
+
+# install chezmoi binary, initialize dotfiles repo, perform all necessary installations
+# curl comes pre-installed with osx
+sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply justinawrey
